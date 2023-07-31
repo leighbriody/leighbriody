900a619c39ed4728ccd870b4eaeaf23b24f4dad7
leigh <lbriody10@gmail.com>
2023-07-31 22:32:53 +0100

    More UI imporvements

--- src/components/Projects/ProjectsGrid.tsx
+++ src/components/Projects/ProjectsGrid.tsx
@@ -3,7 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 
 export function ProjectsGrid() {
   return (
-    <div className="mx-auto max-w-[85rem] bg-primary px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
+    <div className="mx-auto max-w-[82rem] bg-primary px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
           <>
