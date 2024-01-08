0a1df1c61c7f50e91fdc1a3613907bd986601714
leigh <lbriody10@gmail.com>
2024-01-08 19:34:06 +0000

    fix margin issue

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,27 +6,30 @@ import { ContactHero } from "./ContactHero";
 
 export default function Contact() {
   return (
-    <div className="skillset-banner py-20">
-      <div className="banner-inner">
+    <div>
+      {" "}
+      <div className="skillset-banner py-20">
         <ContactHero />
 
-        <section>
-          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-4 lg:py-6">
-            <div className="max-w-screen-xl">
-              <div className="grid grid-cols-3 gap-4 md:grid-cols-5 md:gap-x-20 lg:grid-cols-7 lg:gap-x-20 lg:gap-y-20">
-                <div>
-                  <GithubSocial />
-                </div>
-                <div>
-                  <LinkedInSocial />
-                </div>
-                <div>
-                  <YoutubeSocial />
+        <div className="banner-inner">
+          <section>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-4 lg:py-6">
+              <div className="max-w-screen-xl">
+                <div className="grid grid-cols-3 gap-4 md:grid-cols-5 md:gap-x-20 lg:grid-cols-7 lg:gap-x-20 lg:gap-y-20">
+                  <div>
+                    <GithubSocial />
+                  </div>
+                  <div>
+                    <LinkedInSocial />
+                  </div>
+                  <div>
+                    <YoutubeSocial />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     </div>
   );
--- src/components/Contact/ContactHero.tsx
+++ src/components/Contact/ContactHero.tsx
@@ -1,7 +1,7 @@
 export function ContactHero() {
   return (
     <section>
-      <div className="px-30 mx-auto  grid max-w-screen-xl grid-cols-2 py-8">
+      <div className="px-30 mx-auto  grid max-w-screen-xl grid-cols-2 px-4 sm:px-4">
         <div className="max-w-screen-lg text-white">
           <h2 className="mb-4 text-4xl tracking-tight text-white">Contact</h2>
         </div>
